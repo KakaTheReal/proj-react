f0a73c97a69eb365f69f30b3708eb1ddf0042420
KakaTheReal <difallah.kais@gmail.com>
2022-12-09 01:07:43 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './App.css'
+import Navbar from "./components/navbar"
+
+
+function App() {
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {createBrowserRouter, RouterProvider, Route, Link} from "react-router-dom";
+import Popular from './pages/Popular';
+import TopRated from './pages/TopRated';
+import Upcoming from './pages/Upcoming';
+
+
+const router = createBrowserRouter([
+  {path: "/", element: <Popular/>,}, 
+  {path:"/toprated",element: <TopRated/>},
+  {path:"/upcoming",element: <Upcoming/>}
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
